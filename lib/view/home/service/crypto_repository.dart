76220b43c2d99abaf5eb26e0ddbc01a2c0826fc3
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-04-24 16:07:48 +0300

    Fixed service key problem

--- lib/view/home/service/crypto_repository.dart
+++ lib/view/home/service/crypto_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:cryptocurrency_ranking_app/view/home/model/crypto.dart';
+import 'package:cryptocurrency_ranking_app/view/home/service/key.dart';
 import 'package:http/http.dart' as http;
 
 abstract class ICryptoRepository {
@@ -11,7 +12,7 @@ class CryptoRepository extends ICryptoRepository {
   String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=99&convert=USD";
   Map<String, String> header = {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': 'b899eac9-4ac5-41b7-816e-9b992c79cd85',
+    'X-CMC_PRO_API_KEY': ServiceKeys.coinMarketApiKey,
   };
 
   @override

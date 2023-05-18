beac0edce2354b68b2b51e6057fd75b49b3c951c
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-05-18 15:39:41 +0300

    Created abstract class for image networking

--- lib/view/home/service/icon/icon_network.dart
+++ lib/view/home/service/icon/icon_network.dart
@@ -1,4 +1,9 @@
-class IconNetwork {
+abstract class IIconNetwork {
+  String getIconToNetwork(String id);
+}
+
+class IconNetwork implements IIconNetwork {
+  @override
   String getIconToNetwork(String id) {
     return 'https://s2.coinmarketcap.com/static/img/coins/64x64/$id.png';
   }
